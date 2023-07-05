667b40b1768e0fc117a0a44a2139427a104d1121
Ekaputri17 <ekaputriw1@gmail.com>
2023-07-05 10:52:43 +0700

    menambahkan code

--- cypress/integration/tugas/tugas_headers.test.js
+++ cypress/integration/tugas/tugas_headers.test.js
@@ -5,7 +5,10 @@ describe('Validate Header', () => {
         cy.get('@pokemon').its('headers').its('content-type').should('include','application/json; charset=utf-8')
     });
     it('Berhasil pada Response body', () => {
-        cy.request('GET', 'https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon').then((res) => {
+        cy.request({
+            'method': 'GET', 
+            url: 'https://pokeapi.co/api/v2/pokemon/ditto'
+        }).as('pokemon').then((res) => {
             expect(res.status).to.equal(200)
             expect(res.body.name).to.equal('ditto')
         });
